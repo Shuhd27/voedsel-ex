01fbf32fcd73cd6a4f1bb34d66865befd0e08d7e
Shuhd27 <shuhdalazzani27@gmail.com>
2025-07-02 12:46:15 +0200

    gefixd

--- resources/views/family/index.blade.php
+++ resources/views/family/index.blade.php
@@ -58,8 +58,10 @@
                                     <td class="px-6 py-4 text-sm text-gray-900 border-b">{{ $family->Vertegenwoordiger }}</td>
                                     <td class="px-6 py-4 text-sm border-b">
                                         <a href="{{ route('family.show', $family->id) }}"
-                                            class="w-1/2 h-8 flex justify-center items-center bg-blue-500 text-white rounded hover:bg-blue-600 text-xs">
-                                            Details
+                                            class="w-1/2 h-8 flex justify-center items-center text-blue-500 text-xs">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box" viewBox="0 0 16 16">
+                                                <path d="M8.186 1.113a.5.5 0 0 0-.372 0L1.846 3.5 8 5.961 14.154 3.5zM15 4.239l-6.5 2.6v7.922l6.5-2.6V4.24zM7.5 14.762V6.838L1 4.239v7.923zM7.443.184a1.5 1.5 0 0 1 1.114 0l7.129 2.852A.5.5 0 0 1 16 3.5v8.662a1 1 0 0 1-.629.928l-7.185 2.874a.5.5 0 0 1-.372 0L.63 13.09a1 1 0 0 1-.63-.928V3.5a.5.5 0 0 1 .314-.464z" />
+                                            </svg>
                                         </a>
 
                                     </td>
@@ -71,7 +73,7 @@
                             <!-- home pagina knoop -->
                             <tr>
                                 <td colspan="7" class="px-6 py-4 text-right">
-                                    <a href="{{ url('home') }}" class="text-blue-500 hover:underline">Home</a>
+                                    <a href="{{ route('dashboard') }}" class="px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Home</a>
                                 </td>
                             </tr>
 
